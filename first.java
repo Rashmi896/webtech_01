f283940e5ac0a4086120e50c8839afd305cea832
Rashmi896 <78081648+Rashmi896@users.noreply.github.com>
2022-09-15 23:34:07 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,13 @@
+public class first {
+	public static void main(String[] args) {
+
+		int a, b;
+		a = b = 5;
+		int sum = 0, difference = 0;
+		sum = a + b;
+		difference = a - b;
+
+		System.out.println(sum);
+		System.out.println(difference);
+	}
+}
